ad0eff3c8c5de9623ed211339218e19bf252ec50
Shiro Kawai <shiro@acm.org>
2001-09-29 20:54:16 +0000

    covered most of GLUT stuff
    git-svn-id: https://gauche.svn.sourceforge.net/svnroot/gauche/Gauche-gl/trunk@9 c205566b-fb4e-0410-ab5c-8f0660156277

---
+++ src/gauche-gl.h
@@ -0,0 +1,26 @@
+/*
+ * gauche-gl.h - Gauche GL extension
+ *
+ *  Copyright(C) 2001 by Shiro Kawai (shiro@acm.org)
+ *
+ *  Permission to use, copy, modify, distribute this software and
+ *  accompanying documentation for any purpose is hereby granted,
+ *  provided that existing copyright notices are retained in all
+ *  copies and that this notice is included verbatim in all
+ *  distributions.
+ *  This software is provided as is, without express or implied
+ *  warranty.  In no circumstances the author(s) shall be liable
+ *  for any damages arising out of the use of this software.
+ *
+ *  $Id: gauche-gl.h,v 1.1 2001-09-29 20:54:16 shirok Exp $
+ */
+
+#ifndef GAUCHE_GL_H
+#define GAUCHE_GL_H
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glx.h>
+
+
+#endif /* GAUCHE_GL_H */

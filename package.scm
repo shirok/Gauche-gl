9cdb7712c1548fd5057c70499410c28699fa32a9
Shiro Kawai <shiro@acm.org>
2020-12-12 21:09:16 -1000

    Add package.scm

---
+++ package.scm
@@ -0,0 +1,19 @@
+;;
+;; Gauche-gl package description
+;;
+
+(define-gauche-package "Gauche-gl"
+  :version "0.6"
+  :description "Graphics library binding for Gauche\n\
+                This package provides binginds for OpenGL, GLUT, GLEW and Cg,\
+                plus some additional libraries useful for graphics programming."
+  :require (("Gauche" (>= "0.9.10")))
+  :providing-modules (gl gl.glut gl.math3d 
+                         gl.simple.image gl.simple.viewer
+                         gl.glfw gl.cg)
+  :authors ("Shiro Kawai <shiro@acm.org>")
+  :maintainers ()
+  :licenses ("BSD")
+  :homepage "https://practical-scheme.net/gauche"
+  :repository "https://github.com/shirok/Gauche-gl.git"
+  )

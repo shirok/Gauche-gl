df4aa8fe36cb96adf8d81a577dc28d5f73966ad7
Shiro Kawai <shiro@acm.org>
2021-11-11 22:59:09 -1000

    Conditionally load defcstruct in precomp-ex
    The feature of defcstruct will be supported in Gauche 0.9.11.

--- src/precomp-ex.scm
+++ src/precomp-ex.scm
@@ -4,5 +4,10 @@
 
 (load "precomp")  ;; main is in here
 
-(add-load-path "." :relative)
-(load "defcstruct")
+;; We need to enhance gauche.cgen if we compile with 0.9.11_pre1 or before.
+;; NB: We need some sort of (version<= ...) feature predicate!
+(cond-expand
+ [(not (or gauche-0.9.11_pre2 gauche-0.9.11))
+  (add-load-path "." :relative)
+  (load "defcstruct")]
+ [else])

2e8bb9d8d681d7040d269a7aa441338bb76a6aa2
Shiro Kawai <shiro@acm.org>
2024-10-17 11:24:46 -1000

    Add gl.simple.scene-time
    Make scene time management into a module.  It is generally useful.

--- examples/simple/simple-anim.scm
+++ examples/simple/simple-anim.scm
@@ -6,43 +6,8 @@
 (use gl)
 (use gl.glut)
 (use gl.simple.viewer)
+(use gl.simple.scene-time)
 (use math.const)
-(use scheme.time)
-
-;; Represents absolute time in the scene
-;;  In 'run' mode, current-scene-time consider the real time passed.
-;;  in 'step' mode, current-scene-time is controlled by program.
-(define-class <scene-time> ()
-  ((mode :init-keyword :mode :init-value :run)
-   (start-time :init-form (current-jiffy)) ;jiffies
-   (current-time :init-value 0)))       ;seconds
-
-(define-constant *stepper* 0.22)         ;seconds
-
-(define (reset-scene-time! T)
-  (set! (~ T'start-time) (current-jiffy))
-  (set! (~ T'current-time) 0))
-
-(define (current-scene-time T)
-  (case (~ T'mode)
-    [(:run) (rlet1 t (/. (- (current-jiffy) (~ T'start-time)) (jiffies-per-second))
-              (set! (~ T'current-time) t))]
-    [(:step) (~ T'current-time)]))
-
-(define (step-scene-time! T :optional (delta-unit 1))
-  (when (eq? (~ T'mode) :step)
-    (set! (~ T'current-time)
-          (max 0 (+ (~ T'current-time) (* delta-unit *stepper*))))))
-
-(define (toggle-scene-time-mode! T)
-  (case (~ T'mode)
-    [(:run)  (set! (~ T'mode) :step)]
-    [(:step) (set! (~ T'mode) :run)
-     (set! (~ T'start-time)
-           (- (current-jiffy)
-              (round->exact (* (~ T'current-time) (jiffies-per-second)))))]))
-
-(define *T* (make <scene-time>))
 
 ;; Drawing shape at the current scnee time.
 ;; This sample draws simple spiral motion.  You can change trajectory procedure
@@ -52,7 +17,7 @@
 (define *horizontal-speed* 2pi)  ; read/sec
 
 (define (trajectory)
-  (define T (current-scene-time *T*))
+  (define T (current-scene-time))
   (define dt (/. 32))
   (gl-begin* GL_LINE_STRIP
     (let loop ((i 0))
@@ -76,11 +41,12 @@
 (define (main args)
   (glut-init args)
   (simple-viewer-set-key! #f
-                          #\r (^ _ (reset-scene-time! *T*))
-                          #\space (^ _ (toggle-scene-time-mode! *T*))
-                          #\< (^ _ (step-scene-time! *T* -1))
-                          #\> (^ _ (step-scene-time! *T* 1))
+                          #\r (^ _ (reset-scene-time!))
+                          #\space (^ _ (toggle-scene-time-mode!))
+                          #\< (^ _ (step-scene-time! -1))
+                          #\> (^ _ (step-scene-time! 1))
                           #\? (^ _ (usage)))
+  (set! (~ (default-scene-time)'stepper) 0.15)
   (simple-viewer-display (^[state]
                            (trajectory)
                            (glut-post-redisplay)))
---
+++ lib/gl/simple/scene-time.scm
@@ -0,0 +1,101 @@
+;;;
+;;; simple/scene-time.scm - scene time management
+;;;
+;;;   Copyright (c) 2024  Shiro Kawai  <shiro@acm.org>
+;;;
+;;;   Redistribution and use in source and binary forms, with or without
+;;;   modification, are permitted provided that the following conditions
+;;;   are met:
+;;;
+;;;   1. Redistributions of source code must retain the above copyright
+;;;      notice, this list of conditions and the following disclaimer.
+;;;
+;;;   2. Redistributions in binary form must reproduce the above copyright
+;;;      notice, this list of conditions and the following disclaimer in the
+;;;      documentation and/or other materials provided with the distribution.
+;;;
+;;;   3. Neither the name of the authors nor the names of its contributors
+;;;      may be used to endorse or promote products derived from this
+;;;      software without specific prior written permission.
+;;;
+;;;   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;;;   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;;;   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+;;;   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+;;;   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+;;;   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;;   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;;;   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;;;   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;;   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;;   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+;; This module manages the time in the scene.  Useful if
+;; the scene is time-dependent (e.g. animation)
+
+(define-module gl.simple.scene-time
+  (use scheme.time)
+  (export <scene-time>
+          default-scene-time
+          reset-scene-time!
+          current-scene-time
+          toggle-scene-time-mode!
+          step-scene-time!))
+(select-module gl.simple.scene-time)
+
+;; Represents absolute time in the scene
+;;  In 'run' mode, current-scene-time consider the real time passed.
+;;  in 'step' mode, current-scene-time is controlled by program.
+(define-class <scene-time> ()
+  ((mode :init-keyword :mode :init-value :run)
+   (stepper :init-keyword :stepper :init-value 1.0) ;seconds
+
+   (start-time :init-form (current-jiffy)) ;jiffies
+   (current-time :init-value 0)))       ;seconds
+
+(define *default-scene-time* (make <scene-time>))
+
+(define (default-scene-time) *default-scene-time*)
+
+(define reset-scene-time!
+  (case-lambda
+    [() (reset-scene-time! *default-scene-time* 0.0)]
+    [(arg)
+     (typecase arg
+       [<scene-time> (reset-scene-time! arg 0.0)]
+       [<real> (reset-scene-time! *default-scene-time* arg)])]
+    [(T time-in-sec)
+     (set! (~ T'start-time)
+           (- (current-jiffy)
+              (round->exact (* time-in-sec (jiffies-per-second)))))
+     (set! (~ T'current-time) time-in-sec)]))
+
+(define current-scene-time
+  (case-lambda
+    [() (current-scene-time *default-scene-time*)]
+    [(T)
+     (case (~ T'mode)
+       [(:run) (rlet1 t (/. (- (current-jiffy) (~ T'start-time)) (jiffies-per-second))
+                 (set! (~ T'current-time) t))]
+       [(:step) (~ T'current-time)])]))
+
+(define step-scene-time!
+  (case-lambda
+    [() (step-scene-time! *default-scene-time*)]
+    [(arg)
+     (typecase arg
+       [<scene-time> (step-scene-time! arg 1)]
+       [<real> (step-scene-time! *default-scene-time* arg)])]
+    [(T delta-unit)
+     (when (eq? (~ T'mode) :step)
+       (set! (~ T'current-time)
+             (max 0 (+ (~ T'current-time) (* delta-unit (~ T'stepper))))))]))
+
+(define toggle-scene-time-mode!
+  (case-lambda
+    [() (toggle-scene-time-mode! *default-scene-time*)]
+    [(T)
+     (case (~ T'mode)
+       [(:run)  (set! (~ T'mode) :step)]
+       [(:step) (set! (~ T'mode) :run) (reset-scene-time! T (~ T'current-time))])]))
